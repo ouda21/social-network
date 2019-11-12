1ca13893110925f9cd679ca7d91126877d59a38b
ouda21 <43988124+ouda21@users.noreply.github.com>
2019-11-12 18:03:11 +0300

    Add files via upload

---
+++ libphp-phpmailer/mail.php
@@ -0,0 +1,26 @@
+<?php
+require '/usr/share/php/libphp-phpmailer/src/PHPMailer.php';
+require '/usr/share/php/libphp-phpmailer/src/SMTP.php';
+$mail = new PHPMailer\PHPMailer\PHPMailer(true);
+$mail->setFrom('admin@example.com');
+$mail->addAddress('ahelperbd@gmail.com');
+$mail->Subject = 'Message sent by PHPMailer';
+$mail->Body = 'Hello! use PHPMailer to send email using PHP';
+$mail->IsSMTP();
+$mail->SMTPSecure = 'ssl';
+$mail->Host = 'ssl://smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Port = 465;
+
+//Set your existing gmail address as user name
+$mail->Username = "ouda.wycliffe@com";
+
+//Set the password of your gmail address here
+$mail->Password = '??Dadi??2';
+if(!$mail->send()) {
+  echo 'Email is not sent.';
+  echo 'Email error: ' . $mail->ErrorInfo;
+} else {
+  echo 'Email has been sent.';
+}
+?>
